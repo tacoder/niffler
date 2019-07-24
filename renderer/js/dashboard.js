24cba336ff4e00ff22b6e38a3e353c72984edb38
tacoder <abhinav.singh21093@gmail.com>
2019-07-24 09:04:04 +0530

    Now displaying base transactions

--- renderer/js/dashboard.js
+++ renderer/js/dashboard.js
@@ -11,18 +11,37 @@ const NodeCouchDb = require('node-couchdb');
 const couch = new NodeCouchDb();
 
 
-var username, transactions;
+var username;
 var topNav, fileform;
 
 const supportedFiles=fileProcessors.supportedProcessors;//{0: "icici_credit", 1: "icici_credit_bulk", 2: "icici_debit", 3:"paytm_bank"};
 
+function loadTransactions(cb){
+	couch.mango("nf_base_tx_data", {"selector":{}}).then(({data, headers, status}) => {
+	    // data is json response
+	    // headers is an object with all response headers
+	    // status is statusCode number
+	    console.log(JSON.stringify(data))
+	    cb(null, data.docs);
+	}, err => {
+		cb(err);
+	    // either request error occured
+	    // ...or err.code=EDOCMISSING if document is missing
+	    // ...or err.code=EUNKNOWN if statusCode is unexpected
+	});
+	// userModel.find(function(err,data){
+	// 	if(err) cb(err);
+	// 	else cb(null,data);
+	// });
+}
+
 function start(){
 	username = ipcRenderer.sendSync('fetch-username');
-	transactions = ipcRenderer.sendSync('fetch-transactions');
 	topNav = new Vue({
 		el:"#top-nav",
 		data:{
-			username:username
+			username:username,
+			transactions:transactions
 		}
 	});
 	fileform = new Vue({
@@ -38,7 +57,23 @@ function start(){
 			},
 			processFile: commonProcessor.processFile
 		}
+	});
+	trans = new Vue({
+		el:"#transactions",
+		data:{
+			transactions:[{"DateTime":"ASdfs"}]
+		},
+		methods:{
+			// storeSelectedFileName: function(selectedFile) {
+			// 	fileform.file = selectedFile.target.files[0];
+			// },
+			// processFile: commonProcessor.processFile
+		}
 	})
+	loadTransactions(function(err, data){
+		if (err) alert(err);
+		else trans.transactions = data;
+	});
 }
 
 window.addEventListener("load", start);
